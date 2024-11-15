c2da31a8b225ce7d3a012648529e59c7b96ee39d
ManiGymnast <muthumanikandan2023@gmail.com>
2024-11-15 23:26:21 +0530

    collection page with db connection

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
 import express from "express";
 import bodyParser from "body-parser";
 import env from "dotenv";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 env.config();
 
+const db = new pg.Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'world',
+    password: 'mani',
+    port: 5433,
+});
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+db.connect(err => {
+    if (err) {
+        console.error('Connection error', err.stack);
+    } else {
+        console.log('Connected to PostgreSQL');
+    }
+});
+
 app.get("/",(req,res)=>{
     res.render("index.ejs");
 });
-app.get("/collection",(req,res)=>{
+app.get("/collection",async(req,res)=>{
+    try {
+        const result = await db.query("SELECT * FROM products");
+        
+        res.render("collection.ejs", { products: result.rows });
+    } catch (err) {
+        console.error(err);
+        res.send("Error: " + err);
+    }
+    
 });
 app.get("/about",(req,res)=>{
 });
